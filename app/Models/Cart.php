5d0dd2539cc94bd65eb99ae6fd7b07d0d7e645ad
wmmeg <wmmegal@gmail.com>
2022-06-16 20:36:25 +0300

    creating cart session

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Cart extends Model
 {
     use HasFactory;
+
+    static function booted()
+    {
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
