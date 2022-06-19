7be4f42060bbcc90f073dd684324d82aee8e1e78
wmmeg <wmmegal@gmail.com>
2022-06-19 15:22:04 +0300

    add cart index

--- resources/views/livewire/cart-item.blade.php
+++ resources/views/livewire/cart-item.blade.php
@@ -1,3 +1,31 @@
-<div>
-    {{ $variation->title }}
+<div class="border-b py-3 flex items-start last:border-0 last:pb-0">
+    <div class="w-20 mr-4">
+        <img src="{{ $variation->getFirstMediaUrl('default', 'thumb200x200') }}" class="w-20">
+    </div>
+
+    <div class="space-y-2">
+        <div>
+            <div class="font-semibold text-lg">
+                {{ $variation->formattedPrice() }}
+            </div>
+            <div class="space-y-1">
+                <div>{{ $variation->product->title }}</div>
+
+                <div class="flex items-center text-sm">
+                    Variation specifics
+                </div>
+            </div>
+        </div>
+
+        <div class="flex items-center space-x-4">
+            <div class="text-sm flex items-center space-x-2">
+                <div class="font-semibold">Quantity</div>
+                <input type="text" class="text-sm w-10" value="{{ $variation->quantity }}">
+            </div>
+
+            <button class="text-sm">
+                Remove
+            </button>
+        </div>
+    </div>
 </div>
